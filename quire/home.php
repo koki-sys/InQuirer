5c4bfe20aaac9494f336a77b621007c69e6170fb
koki-sys <soraisu7@gmail.com>
2020-11-30 13:32:15 +0900

    finish geoip

--- quire/home.php
+++ quire/home.php
@@ -1,6 +1,27 @@
 <?php require 'header.php'; ?>
 <div class="container">
   <h4>現在地付近の書籍</h4>
+  <?php
+
+  /* 本番環境でしか使えない
+  function grabIpInfo($ip)
+  {
+
+    $curl = curl_init();
+
+    curl_setopt($curl, CURLOPT_URL, "https://api.ipgeolocationapi.com/geolocate/" . $ip);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+    $returnData = curl_exec($curl);
+    curl_close($curl);
+    return $returnData;
+  }
+  $ipInfo = grabIpInfo($_SERVER["REMOTE_ADDR"]);
+  $ipJsonInfo = json_decode($ipInfo);
+
+  echo $ipJsonInfo->name;
+  */
+
+  ?>
   <div class="row">
     <div class="col-0 col-md-1 col-lg-0">
       <div class="col-md-5 col-lg-4">
