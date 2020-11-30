9307f3670123e6c4e54f9ec80e772e13654878a2
koki-sys <soraisu7@gmail.com>
2020-11-30 12:00:56 +0900

    responsive

--- quire/login.php
+++ quire/login.php
@@ -5,26 +5,31 @@
         <span class="rounded-circle p-3" style="font-size: 3em;">x</span>
       </a><br>
     </li>
-    <li><img src="../img/login.png" alt="login" class="float-right" width="70%" height="70%" style="margin-top: 15%; margin-right: 25%"></li>
   </ul>
 
-  <div class="ml-5" style="margin-top: 8%">
-    <h3>サインイン</h3>
-    <label class="mt-2">新規ユーザの方</label>
-    <a href="register.php">アカウントの作成</a>
+  <div style="margin-top: 8%">
+    <div class="row">
+      <div class="col-md-1"></div>
+      <div class="col-md-6">
+        <h3>サインイン</h3>
+        <label class="mt-2">新規ユーザの方</label>
+        <a href="register.php">アカウントの作成</a>
+      </div>
+      <div class="col-md-5"></div>
+    </div>
     <form action="home.php" method="post">
-      <div class="form-group mt-3">
-        <div class="form-row">
-          <div class="col-12 col-md-6">
-            <input type="text" name="name" placeholder="ユーザ名を入力" class="form-control"><br>
-          </div>
-          <div class="col-md-6"></div>
-          <div class="col-12 col-md-6">
-            <input type="password" name="password" placeholder="パスワード" class="form-control"><br>
-          </div>
-          <div class="col-md-6"></div>
-          <div class="col-12 col-md-6">
-            <input type="submit" value="サインイン" class="pt-2 pb-2 bg-dark text-white btn-block">
+      <div class="form-group mt-5">
+        <div class="col-12 col-md-6 float-right">
+          <img src="../img/login.png" alt="login" class="mx-auto d-block" width="60%" height="60%">
+        </div>
+        <div class="col-12 col-md-6">
+          <div class="row">
+            <div class="col-md-1"></div>
+            <div class="col-md-11">
+              <input type="text" name="name" placeholder="ユーザ名を入力" class="form-control"><br>
+              <input type="password" name="password" placeholder="パスワード" class="form-control"><br>
+              <input type="submit" value="サインイン" class="pt-2 pb-2 bg-dark text-white btn-block">
+            </div>
           </div>
         </div>
       </div>
--- quire/register.php
+++ quire/register.php
@@ -9,10 +9,16 @@
     </li>
   </ul>
 
-  <div class="ml-5" style="margin-top: 8%">
-    <h3>アカウントの作成</h3>
-    <label class="mt-2">アカウントをお持ちの方</label>
-    <a href="login.php">サインイン</a>
+  <div style="margin-top: 8%;">
+    <div class="row">
+      <div class="col-md-1"></div>
+      <div class="col-md-6">
+        <h3>アカウントの作成</h3>
+        <label class="mt-2">アカウントをお持ちの方</label>
+        <a href="login.php">サインイン</a>
+      </div>
+      <div class="col-md-5"></div>
+    </div>
     <form action="home.php" method="post">
       <div class="form-group mt-5">
         <!--php-->
@@ -27,18 +33,21 @@
         echo '<img src="../img/register.png" alt="login" class="mx-auto d-block" width="60%" height="60%">';
         echo '</div>';
         echo '<div class="col-12 col-md-6">';
+        echo '<div class="row">';
+        echo '<div class="col-md-1"></div>';
+        echo '<div class="col-md-11">';
         echo '<input type="text" name="name" placeholder="ユーザ名" class="form-control"><br>';
         echo '<input type="text" name="email" placeholder="Eメール" class="form-control"><br>';
         echo '<input type="password" name="password" placeholder="パスワード" class="form-control"><br>';
+        echo '<input type="submit" value="サインアップ" class="pt-2 pb-2 bg-dark text-white btn-block">';
+        echo '</div>';
+        echo '</div>';
         echo '</div>';
         ?>
         <!--php_end-->
-        <div class="col-md-6"></div>
-        <div class="col-12 col-md-6">
-          <input type="submit" value="サインアップ" class="pt-2 pb-2 bg-dark text-white btn-block">
-        </div>
       </div>
+    </form>
   </div>
-  </form>
+</div>
 </div>
 <?php require '../footer.php'; ?>
