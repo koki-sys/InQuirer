09395c11c3578ec06d49d6081bb8d7894ffc144a
koki-sys <soraisu7@gmail.com>
2020-12-01 11:21:46 +0900

    folder rename

--- component/pdo.php
+++ component/pdo.php
@@ -1,3 +1,8 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=inquirer;charset=utf8', 'soraisu', 'sprwAeixb26vds');
-?>
+try {
+  $pdo = new PDO('mysql:host=localhost;dbname=inquirer;charset=utf8', 'soraisu', 'sprwAeixb26vds');
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  echo $e->getMessage();
+  exit;
+}
