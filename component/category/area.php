1efa67f4a3327327e660c7be32df633d4ad79c32
koki-sys <soraisu7@gmail.com>
2020-12-02 12:19:31 +0900

    refactor component

--- component/category/area.php
+++ component/category/area.php
@@ -1,11 +1,9 @@
-<?php
-foreach ($sql as $row) {
-  echo '<form action="area.php" method="post">';
-  echo '<li class="list-group-item text-center" style="background-color: #fbfbfb;">';
-  echo '<input type="hidden" name="areaid" value="', $row['id'], '">';
-  echo '<input type="hidden" name="area" value="', $row['name'], '">';
-  echo '<input type="submit" class="text-dark btn btn-white" value="', $row['name'],'">';
-  echo '</li>';
-  echo '</form>';
-}
-?>
+<?php foreach ($sql as $row) :?>
+  <form action="area.php" method="post">
+  <li class="list-group-item text-center" style="background-color: #fbfbfb;">
+  <input type="hidden" name="areaid" value="<?php echo $row['id']; ?>">
+  <input type="hidden" name="area" value="<?php echo $row['name']; ?>">
+  <input type="submit" class="text-dark btn btn-white" value="<?php echo $row['name']; ?>">
+  </li>
+  </form>
+<?php endforeach; ?>
--- component/category/genre.php
+++ component/category/genre.php
@@ -1,11 +1,9 @@
-<?php
-foreach ($sql as $row) {
-  echo '<form action="genre.php" method="post">';
-  echo '<li class="list-group-item text-center" style="background-color: #fbfbfb;">';
-  echo '<input type="hidden" name="genreid" value="', $row['id'], '">';
-  echo '<input type="hidden" name="genre" value="', $row['name'], '">';
-  echo '<input type="submit" class="text-dark btn btn-white" value="', $row['name'],'">';
-  echo '</li>';
-  echo '</form>';
-}
-?>
+<?php foreach ($sql as $row) : ?>
+  <form action="genre.php" method="post">
+    <li class="list-group-item text-center" style="background-color: #fbfbfb;">
+      <input type="hidden" name="areaid" value="<?php echo $row['id']; ?>">
+      <input type="hidden" name="area" value="<?php echo $row['name']; ?>">
+      <input type="submit" class="text-dark btn btn-white" value="<?php echo $row['name']; ?>">
+    </li>
+  </form>
+<?php endforeach; ?>
