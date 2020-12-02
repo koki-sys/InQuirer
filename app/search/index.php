60646eb86677dfdde652145f02279bcceb1dc34e
koki-sys <soraisu7@gmail.com>
2020-12-02 12:13:10 +0900

    refactor search

--- app/search/index.php
+++ app/search/index.php
@@ -1,7 +1,7 @@
 <?php require '../../component/header/header.php'; ?>
 <div class="container">
   <h4>
-  <?php echo $_POST['search']; ?>
-  の検索結果</h4>
+    <?php echo $_POST['search']; ?>の検索結果
+  </h4>
 </div>
 <?php require '../../component/footer/footer_content.php'; ?>
