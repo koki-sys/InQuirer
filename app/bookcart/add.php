42103095141ccb66304337be515fa26145c9f93a
koki-sys <soraisu7@gmail.com>
2020-12-06 13:33:52 +0900

    bookcart component

--- app/bookcart/add.php
+++ app/bookcart/add.php
@@ -34,19 +34,7 @@ $cartid = $_POST['cartid'] ?? '';
     </div>
     <h6 class="mt-3">ブックカートに入っているもの</h6>
     <div class="row">
-      <?php
-      $index = $pdo->prepare('SELECT * FROM rental WHERE customer_id = ? AND rental_flg = 0');
-      $index->execute([$_SESSION['customer']['id']]);
-      foreach ($index as $rental2) {
-        $bookimg = $pdo->prepare('SELECT * FROM book WHERE id = ?');
-        $bookimg->execute([$rental2['book_id']]);
-        foreach ($bookimg as $img) {
-          echo '<div class="col-3 m-2">';
-          echo '<img src="../../img/book_img/', $img['img'], '" alt="img" height="80%" width="auto">';
-          echo '</div>';
-        }
-      }
-      ?>
+      <?php require '../../component/book/bookcart.php'; ?>
     </div>
   </div>
 
