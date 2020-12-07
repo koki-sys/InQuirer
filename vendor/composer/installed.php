e9e67d91f7c253898b512d5b0f861b51a9b20377
koki-sys <soraisu7@gmail.com>
2020-12-07 18:23:54 +0900

    add composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '7e66e894561e7e8796c96051a335584dbea70769',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7e66e894561e7e8796c96051a335584dbea70769',
+    ),
+  ),
+);
