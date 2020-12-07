708a23002cce39e1417d5a72aa0cdacb6684a31c
koki-sys <soraisu7@gmail.com>
2020-12-07 11:31:59 +0900

    commit

---
+++ component/component.php
@@ -0,0 +1,19 @@
+<!-- component/header -->
+<?php require '../../component/header/header.php'; ?>
+<?php require '../../component/header/auth_header.php'; ?>
+<?php require '../../component/header/logined_header.php'; ?>
+<?php require '../../component/header/nologin_header.php'; ?>
+
+<!-- component/footer -->
+<?php require '../../component/footer/footer.php'; ?>
+
+<!-- component/book -->
+<?php require '../../component/book/bookcart.php'; ?>
+<?php require '../../component/book/card.php'; ?>
+
+<!-- component/category -->
+<?php require '../../component/category/area.php'; ?>
+<?php require '../../component/category/genre.php'; ?>
+
+<!-- pdo -->
+<?php require '../../component/pdo.php'; ?>
