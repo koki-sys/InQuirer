cbf4bdaf1959583146a98a52ce2ff2169cd43955
koki-sys <soraisu7@gmail.com>
2020-12-08 11:24:05 +0900

    bug fix

--- component/book/rmaxcnt.php
+++ component/book/rmaxcnt.php
@@ -7,5 +7,7 @@ $maxcnt->execute([$_SESSION['customer']['id']]);
     <h4 class="text-danger mt-5 ml-5">貸出上限を超えています。</h4>
     <h5 class="text-danger mt-2 ml-5">現在貸出冊数：<?php echo $mc['cnt']; ?></h5>
     <?php exit; ?>
+  <?php elseif($mc['cnt'] == 0): ?>
+    <h5 class="mt-5 text-center">予約した書籍はありません。</h5>
   <?php endif; ?>
 <?php endforeach; ?>
